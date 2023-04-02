81d60f55ec77aa7120e267890c093a85a513ac03
Pavlov Ivan <pivaq4530@gmail.com>
2023-04-02 19:48:09 +0300

    add base things

---
+++ Data/lexica.py
@@ -0,0 +1,9 @@
+START = '''Welcome! Send /photo to get a random photo of cat or 
+/help to get full description of the bot'''
+HELP = '''Commands: 
+       1) start - turn on bot
+       2) photo - get the random photo
+       3) like - add photo to your favorites 
+       4) watchlike - see your favorites 
+       5) help - full description'''
+PHOTO = ['Perfect, right?', 'Look at that cutie!!', 'Do you like this little fella?', 'Adorable!!']
--- bot.py
+++ bot.py
@@ -1,2 +1,16 @@
-from Data.config import Token
+from aiogram import Bot, Dispatcher, types
+from Data.config import TOKEN
+from aiogram.filters import Command
+from Data.lexica import START
 
+bot = Bot(TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command(commands=['start']))
+async def echo(message: types.Message):
+    await message.reply(START)
+
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
