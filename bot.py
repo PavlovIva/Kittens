6a26019cd204550cfcd0d4d2955860f664fb5d90
Pavlov Ivan <pivaq4530@gmail.com>
2023-04-03 11:59:59 +0300

    fix security problems and clean the code

--- bot.py
+++ bot.py
@@ -1,25 +1,14 @@
+# Импортируем все необходимое
 from aiogram import Bot, Dispatcher, types
-from Data.config import TOKEN, url_for_kittens, data, admin
+from Data.config import TOKEN
 from aiogram.filters import Command, Text, or_f
 from Data.lexica import START, HELP
-import requests
-import random
+from funcs import get_a_cat
 
+
+# Объявляем необходимые переменные
 bot = Bot(TOKEN)
 dp = Dispatcher()
-
-def get_a_cat():
-    response = requests.get('https://api.telegram.org/bot5873257022:AAHCkfdP4bJAV3oQhB9FRNqnbZoiRtpUdKc/sendPhoto?chat_id=867978028&photo=https://cataas.com/cat')
-    response.raise_for_status()
-    #return response.json()['file']
-
-
-def get_a_cat1():
-    url = 'https://cataas.com/cat?json=true'
-    response = requests.get(url)
-    response.raise_for_status()
-    return response.json()['file']
-
 
 
 @dp.message(Command(commands=['start']))  # Реагирует на комманду старт
@@ -32,15 +21,10 @@ async def help_func(message: types.Message):
     await message.reply(HELP)
 
 
+# Прототип. Сейчас отправляет одно и то же фото через API
 @dp.message(or_f(Command(commands=['photo']), Text(startswith=['Хочу котика', 'Давай кота'], ignore_case=True)))
-async def kittens(message: types.Message):
+async def kittens(message):
     get_a_cat()
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
