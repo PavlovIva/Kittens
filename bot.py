8c4e9f5e564b9130d5be10f78af13d5afd667a6a
Pavlov Ivan <pivaq4530@gmail.com>
2023-04-05 09:43:06 +0300

    Started working on adding favorite pics. Success!

--- bot.py
+++ bot.py
@@ -1,16 +1,15 @@
 # Импортируем все необходимое
 from aiogram import Bot, Dispatcher, types
-from Data.config import TOKEN
+from Data.config import TOKEN, message_ids
 from aiogram.filters import Command, Text, or_f
 from Data.lexica import START, HELP
-from funcs import get_a_cat
-
+from funcs import GetCat
 
 
 # Объявляем необходимые переменные
 bot = Bot(TOKEN)
 dp = Dispatcher()
-n = 0
+
 
 
 @dp.message(Command(commands=['start']))  # Реагирует на комманду старт
@@ -26,8 +25,22 @@ async def help_func(message: types.Message):
 # Прототип. Работает и отправляет фото!!! Через API
 @dp.message(or_f(Command(commands=['photo']), Text(startswith=['Хочу котика', 'Давай кота'], ignore_case=True)))
 async def kittens(message: types.Message):
-    get_a_cat(message_from_user_id=message.from_user.id)
+    get_a_catt = GetCat()
+    get_a_catt.get_a_cat(message_from_user_id=message.from_user.id)
+    idd = get_a_catt.photo_id()
+    message_ids.append(idd)
 
+
+# По плану, должно добавлять фото в избранные.
+@dp.message(Command(commands='like'))
+async def like(message: types.message.Message):
+    pass
+
+
+# Отправляет понравившиеся фото
+@dp.message(Command(commands='watchlike'))
+async def watchlike(message: types.Message):
+    await bot.forward_message(message.from_user.id, message.from_user.id, message_ids[0])
 
 
 if __name__ == '__main__':
--- funcs.py
+++ funcs.py
@@ -1,9 +1,8 @@
+# Импорт всего необходимого
 import requests
 import random
 from Data.lexica import PHOTO
-from Data.config import TOKEN
-
-
+from Data.config import TOKEN, admin
 
 
 # Получает id фото
@@ -11,14 +10,24 @@ def get_a_cat1():
     url = 'https://cataas.com/cat?json=true'
     response = requests.get(url)
     response.raise_for_status()
-    return response.json()['file']
+    file = response.json()['file']
+    requests.get(f'https://api.telegram.org/bot{TOKEN}/sendPhoto?chat_id={admin}&photo=http://random.cat/view/{file}')
 
 
-# Отправляет фото кота боту через API
-def get_a_cat(message_from_user_id):
-    caption = PHOTO[random.randint(0, (len(PHOTO) - 1))]
-    response = requests.get(f'https://api.telegram.org/bot{TOKEN}/sendPhoto?chat_id={message_from_user_id}&caption={caption}&photo=http://random.cat/view/{str(random.randint(0, 1677))}')
-    response.raise_for_status()
+# Класс, отвечающий за отправку фото, получения айди сообщения.
+class GetCat:
 
+    def __init__(self):
+        self.message_id = 0
 
+    # Отправка фото через Telegram API
+    def get_a_cat(self, message_from_user_id):
+        caption = PHOTO[random.randint(0, (len(PHOTO) - 1))]
+        response = requests.get(f'https://api.telegram.org/bot{TOKEN}/sendPhoto?chat_id={message_from_user_id}&caption={caption}&photo=http://random.cat/view/{str(random.randint(0, 1677))}')
+        response.raise_for_status()
+        self.message_id = (response.json()['result']['message_id'])
+
+    # Возвращение айди сообщения
+    def photo_id(self):
+        return self.message_id
 
